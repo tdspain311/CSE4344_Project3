e1f570fcb686b327e58dd9bfd9fc0022d506627d
krishnamanibhattarai <krishnamanibhattarai@mavs.uta.edu>
2015-11-16 16:38:01 -0600

    [Created Directory Source Files]

---
+++ Source Files/main.py
@@ -0,0 +1,22 @@
+"""
+Krishna Bhattarai
+Computer Networks, CSE 4345
+Project #3
+November 16, 2015
+Implementation of the Distance Vector Routing Protocol
+References: Page 371-379 from the book
+
+"""
+
+
+def main():
+    """
+    This is our main function. This is where our program will start.
+    :return: None
+    """
+    pass
+
+# Invoke the main function here
+if __name__ == "__main__":
+    main()
+
